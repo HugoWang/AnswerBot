131e9321836bcf7109854ffadd1833cfd8fad246
HugoWang <wk.oulu@gmail.com>
2015-10-06 13:04:29 +0300

    test

--- app/src/main/java/com/example/kaiwang/answerbot/EntryActivity.java
+++ app/src/main/java/com/example/kaiwang/answerbot/EntryActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class GetRecomendActivity extends AppCompatActivity {
+public class EntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_recomend);
+        setContentView(R.layout.activity_entry);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_get_recomend, menu);
+        getMenuInflater().inflate(R.menu.menu_entry, menu);
         return true;
     }
 
