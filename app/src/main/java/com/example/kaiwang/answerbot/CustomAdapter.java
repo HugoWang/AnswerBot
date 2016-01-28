e795e9e63f7e3bcfb9fa4f58254bd37b3b19afa3
feilz <akerlunddan@gmail.com>
2016-01-28 10:25:29 +0200

    A search has been implemented, removed the button on main page. The search is still unfinished.

--- app/src/main/java/com/example/kaiwang/answerbot/CustomAdapter.java
+++ app/src/main/java/com/example/kaiwang/answerbot/CustomAdapter.java
@@ -3,16 +3,11 @@ package com.example.kaiwang.answerbot;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -28,12 +23,18 @@ class CustomAdapter extends ArrayAdapter<Questions>{
         if(convertView==null){
             convertView=LayoutInflater.from(getContext()).inflate(R.layout.activity_home_question,parent,false);
         }
-        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),"RobotoCondensed-Regular.ttf");
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "RobotoCondensed-Regular.ttf");
         Questions plQuestion = getItem(position);
         TextView MainQuestion = (TextView) convertView.findViewById(R.id.qquestion);
         String question= plQuestion.question_body;
         MainQuestion.setText(question);
         MainQuestion.setTypeface(tf);
+        if (plQuestion.sight){
+            convertView.setVisibility(convertView.VISIBLE);
+        }else{
+            convertView.setVisibility(convertView.GONE);
+        }
+
         return convertView;
     }
 }
