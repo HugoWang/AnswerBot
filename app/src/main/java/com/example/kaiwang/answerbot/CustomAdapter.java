9dbd11f504f5bd224d06ee4b0c7a0577cebe0b8f
feilz <akerlunddan@gmail.com>
2016-01-29 14:35:18 +0200

    Search added, a working version

--- app/src/main/java/com/example/kaiwang/answerbot/CustomAdapter.java
+++ app/src/main/java/com/example/kaiwang/answerbot/CustomAdapter.java
@@ -19,21 +19,22 @@ class CustomAdapter extends ArrayAdapter<Questions>{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         if(convertView==null){
             convertView=LayoutInflater.from(getContext()).inflate(R.layout.activity_home_question,parent,false);
         }
-        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "RobotoCondensed-Regular.ttf");
         Questions plQuestion = getItem(position);
+        if (plQuestion.sight){
+            convertView.setVisibility(View.VISIBLE);
+        }else{
+            convertView.setVisibility(View.GONE);
+        }
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "RobotoCondensed-Regular.ttf");
+
         TextView MainQuestion = (TextView) convertView.findViewById(R.id.qquestion);
         String question= plQuestion.question_body;
         MainQuestion.setText(question);
         MainQuestion.setTypeface(tf);
-        if (plQuestion.sight){
-            convertView.setVisibility(convertView.VISIBLE);
-        }else{
-            convertView.setVisibility(convertView.GONE);
-        }
+
 
         return convertView;
     }
