b05198b7b9c418d9e8497299b579e83162e4bbda
Kiminorman <Kimi.Norman@student.oulu.fi>
2016-02-11 09:07:48 +0200

    added bundle handling to Add screens

--- app/src/main/java/com/example/kaiwang/answerbot/AddOptionsActivity.java
+++ app/src/main/java/com/example/kaiwang/answerbot/AddOptionsActivity.java
@@ -6,7 +6,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.os.BaseBundle;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -17,17 +16,18 @@ public class AddOptionsActivity extends AppCompatActivity {
     String question_body = "Passed Question Body";
     String question_id = "Passed Question ID";
     String user_id = "Passed User_ID";
-/*
-    if(!bundle.isEmpty()){
-        question_id = bundle.getString("question_id");
-        question_body = bundle.getString("question_body");
-        user_id = bundle.getString("user_id");
-    }*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_options);
         TextView QuestionTextView = (TextView) findViewById(R.id.AddOptionsQuestionTextView);
+        Bundle bundle = getIntent().getExtras();
+        if(!bundle.isEmpty()) {
+            question_id = Integer.toString(bundle.getInt("position"));
+            question_body = bundle.getString("question_body");
+            user_id = bundle.getString("user_id");
+        }
         QuestionTextView.setText(question_body);
     }
 
